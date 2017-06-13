bed834553d868ece884409a21e988565ab27c728
Meryl Turner <merylturner08@gmail.com>
2017-06-13 14:29:22 -0700

    got data from github and added to our repo.all array

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -12,17 +12,23 @@ var app = app || {};
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
     $.ajax({
-      url: `https://api.gihub.com/user/repos`,
+      url: `https://api.github.com/user/repos`,
       type: 'GET',
       headers: {'Authorization': `token ${githubToken}`} // eslint-disable-line
     })
-    .then(data => repos.all = data, err => console.error(err)) 
+    .then(data => repos.all = data, err => console.error(err))
+    // .then(function(data,err){
+    //   if(data) {repos.all.push(data);}
+    //   else{console.error(err)}
+    // })
     .then(callback);
+    
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
   repos.with = attr => repos.all.filter(repo => repo[attr]);
+  console.log('after repos.with', repos.all);
 
   module.repos = repos;
 })(app);
